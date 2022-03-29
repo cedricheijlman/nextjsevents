1e29af4122df6fb56a1987485b22eb7c43a861b4
cedricheijlman <90176813+cedricheijlman@users.noreply.github.com>
2022-03-29 19:36:59 +0200

    filtered events

--- components/ui/button.js
+++ components/ui/button.js
@@ -1,9 +1,16 @@
 import React from "react";
-
+import Link from "next/link";
 import styles from "./Button.module.css";
 
-const Button = ({ children }) => {
-  return <button className={styles.button}>{children}</button>;
+const Button = ({ children, link }) => {
+  // if no link
+  if (!link) return <button className={styles.button}>{children}</button>;
+
+  return (
+    <Link href={link && link}>
+      <button className={styles.button}>{children}</button>
+    </Link>
+  );
 };
 
 export default Button;
--- pages/events/[...slug].js
+++ pages/events/[...slug].js
@@ -1,7 +1,61 @@
+import { useRouter } from "next/router";
+import { Fragment } from "react";
+import Button from "../../components/ui/button";
+import Link from "next/link";
+import { getFilteredEvents } from "../../dummy-data";
+import { EventList } from "../../components/events/eventList";
+
+import styles from "../../styles/FilteredEvents.module.css";
+
 const filteredEventsPage = () => {
+  const router = useRouter();
+
+  const filterData = router.query.slug;
+
+  if (!filterData) {
+    return <p className="center">Loading...</p>;
+  }
+  console.log(filterData);
+
+  const filteredYear = filterData[0];
+  const filteredMonth = filterData[1];
+
+  const numYear = +filteredYear;
+  const numMonth = +filteredMonth;
+
+  if (
+    isNaN(numYear) ||
+    isNaN(numMonth) ||
+    numYear > 2030 ||
+    numMonth < 1 ||
+    numMonth > 12
+  ) {
+    return (
+      <Fragment>
+        <div className={styles.container}>
+          <Button link={"/events"}>Show All Events</Button>
+        </div>
+      </Fragment>
+    );
+  }
+
+  const filteredEvents = getFilteredEvents({ year: numYear, month: numMonth });
+
+  if (!filteredEvents || filteredEvents.length === 0) {
+    return (
+      <Fragment>
+        <div className={styles.container}>
+          <h2>No events found for the chosen filter!</h2>
+          <Button link={"/events"}>Show All Events</Button>
+        </div>
+      </Fragment>
+    );
+  }
+
   return (
-    <div>
+    <div className={styles.container}>
       <h2>Filtered Events</h2>
+      <EventList items={filteredEvents} />
     </div>
   );
 };

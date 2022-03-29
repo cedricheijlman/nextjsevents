9260cd13cea74d26b333fd36945b57190c48715b
Cedric Heijlman <90176813+cedricheijlman@users.noreply.github.com>
2022-03-29 13:22:51 +0200

    event details page

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,11 +1,33 @@
 import styles from "../../styles/EventDetailsPage.module.css";
+import { useRouter } from "next/router";
+import { getEventById } from "../../dummy-data";
+import EventIcon from "@mui/icons-material/Event";
+import LocationOnIcon from "@mui/icons-material/LocationOn";
 const eventDetailsPage = () => {
+  let router = useRouter();
+
+  const eventId = router.query.eventId;
+  let event = getEventById(eventId);
+
+  if (!event) {
+    return <p>No event found!</p>;
+  }
+  console.log(event.image);
   return (
     <div className={styles.container}>
+      <h2 className={styles.title}>{event.title}</h2>
       <div className={styles.eventInfo}>
-        <h2 className={styles.title}>Title Event</h2>
-        <p>Description</p>
+        <img src={event.image} height={250} width={250} alt={"not working"} />
+        <div>
+          <div>
+            <EventIcon />
+          </div>
+          <div>
+            <LocationOnIcon />
+          </div>
+        </div>
       </div>
+      <p>{event.description}</p>
     </div>
   );
 };
